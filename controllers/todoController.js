db424c89705b9ebef6228d85876f024810eb2624
Kapil Chopra <kapilchopra963@gmail.com>
2018-06-16 19:59:10 -0400

    Linked CRUD with MongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -11,10 +11,10 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model('Todo', todoSchema);
 
-var itemOne = Todo({item: 'read book'}).save(function(err){
-  if(err) throw err;
-  console.log('item saved');
-});
+// var itemOne = Todo({item: 'read book'}).save(function(err){
+//   if(err) throw err;
+//   console.log('item saved');
+// });
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'make todo application'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -22,19 +22,34 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 module.exports = function(app){
 
 app.get('/todo', function(req, res){
-  res.render('todo', {todos: data});
+  //get data from mongodb and pass it to view
+  Todo.find({}, function(err, data){
+    if(err) throw err;
+    res.render('todo', {todos: data});
+  });
+  // res.render('todo', {todos: data});
 });
 
 app.post('/todo', urlencodedParser, function(req, res){
-  data.push(req.body);
-  res.json(data);
+  //get data from the view to mongodb
+  var newTodo = Todo(req.body).save(function(err, data){
+    if(err) throw err;
+    res.json(data);
+  });
+  // data.push(req.body);
+  // res.json(data);
 });
 
 app.delete('/todo/:item', function(req, res){
-  data = data.filter(function(todo){
-    return todo.item.replace(/ /g, '-') !== req.params.item;
+  //delete the requested item from mongodb
+  Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+    if(err) throw err;
+    res.json(data);
   });
-  res.json(data);
+  // data = data.filter(function(todo){
+  //   return todo.item.replace(/ /g, '-') !== req.params.item;
+  // });
+  // res.json(data);
 });
 
 };
